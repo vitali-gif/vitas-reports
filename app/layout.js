8d30282a26a5dec2273671b02eed2096a27b7ccf
Vitali <vitalidisel@gmail.com>
2026-06-01 21:14:23 +0000

    feat: add Microsoft Clarity tracking (x0evra1juu)

--- app/layout.js
+++ app/layout.js
@@ -15,6 +15,11 @@ export default function RootLayout({ children }) {
           href="https://fonts.googleapis.com/css2?family=Heebo:wght@300;400;500;600;700;800;900&display=swap"
           rel="stylesheet"
         />
+        {/* Microsoft Clarity */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{ __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","x0evra1juu");` }}
+        />
       </head>
       <body>{children}</body>
     </html>
